efed73fc3da393d7bceb4b97cc077f18b1a51316
TrongHieu1998 <54543884+TrongHieu1998@users.noreply.github.com>
2020-05-25 15:47:43 +0700

    3:47 25/5

---
+++ QuanLyKhuTro/BLL/BLL_DichVu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+namespace BLL
+{
+    public class BLL_DichVu
+    {
+        DAL_DichVu dichvu = new DAL_DichVu();
+        public List<DICHVU> loadBangDV()
+        {
+            return loadBangDV();
+        }
+        //kiểm tra khóa chính
+        public bool ktkc_DichVu(string pMa)
+        {
+            return dichvu.ktakhoachinh_DichVu(pMa);
+        }
+        //Thêm
+        public bool them_DichVu(DICHVU pDichVu)
+        {
+            return dichvu.them_DichVu(pDichVu);
+        }
+        //ktra xóa
+        public bool ktx_dv(String dv)
+        {
+            return dichvu.kt_XoaDV(dv);
+        }
+        //Xóa
+        public bool xoa_DichVu(string pMa)
+        {
+            return dichvu.xoa_DichVu(pMa);
+        }
+        //Sửa
+        public bool sua_DichVu(DICHVU pDichVu)
+        {
+            return dichvu.sua_DichVu(pDichVu);
+        }
+    }
+}
---
+++ QuanLyKhuTro/DAL/DAL_DichVu.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DAL_DichVu
+    {
+        QL_KhuTroDataContext data = new QL_KhuTroDataContext();
+
+        //lấy tất cả dữ liệu
+        public List<DICHVU> loadbangDichVu()
+        {
+            var dulieu = (from s in data.DICHVUs select s);
+            return dulieu.ToList<DICHVU>();
+        }
+
+        //kiểm tra khóa chính
+        public bool ktakhoachinh_DichVu(string hd)
+        {
+            var kt = (from h in data.DICHVUs
+                      where h.MADV == hd
+                      select h).Count();
+            if (kt > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Thêm
+        public bool them_DichVu(DICHVU dv)
+        {
+            try
+            {
+                data.DICHVUs.InsertOnSubmit(dv);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        //Xóa
+
+        public bool xoa_DichVu(string pMaDV)
+        {
+            try
+            {
+                DICHVU dv = data.DICHVUs.Where(t => t.MADV == pMaDV).FirstOrDefault();
+                data.DICHVUs.DeleteOnSubmit(dv);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //Sửa
+        public bool sua_DichVu(DICHVU pDichVu)
+        {
+            try
+            {
+                DICHVU nv = data.DICHVUs.Where(t => t.MADV == pDichVu.MADV).FirstOrDefault();
+                if (nv != null)
+                {
+                    nv.TENDV = pDichVu.TENDV;
+                    nv.GIADV = pDichVu.GIADV;
+                    nv.DONVI = pDichVu.DONVI;
+                    data.SubmitChanges();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        //kiểm tra tầng có đang được sử dụng
+        public bool kt_XoaDV(string hd)
+        {
+
+            var ktx = (from t in data.DICHVUs
+                       from p in data.DICHVUs
+                       where t.MADV == hd && p.MADV == hd
+                       select t).Count();
+            if (ktx > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
