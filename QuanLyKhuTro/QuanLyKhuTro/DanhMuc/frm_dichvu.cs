4bb733b0e6298f55aa87b04bc8faa7175e4a45fd
TrongHieu1998 <54543884+TrongHieu1998@users.noreply.github.com>
2020-05-25 17:56:32 +0700

    5:56

--- QuanLyKhuTro/QuanLyKhuTro/DanhMuc/frm_dichvu.cs
+++ QuanLyKhuTro/QuanLyKhuTro/DanhMuc/frm_dichvu.cs
@@ -9,12 +9,13 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
 using BLL;
+using DAL;
 
 namespace QuanLyKhuTro.DanhMuc
 {
     public partial class frm_dichvu : DevExpress.XtraEditors.XtraUserControl
     {
-        BLL_DichVu dv = new BLL_DichVu();
+        BLL_DichVu dichvu = new BLL_DichVu();
         public frm_dichvu()
         {
             InitializeComponent();
@@ -27,7 +28,7 @@ namespace QuanLyKhuTro.DanhMuc
         private void frm_dichvu_Load(object sender, EventArgs e)
         {
             txt_madichvu.Enabled = false;
-            grv_dichvu.DataSource = dv.loadBang_DV();
+            grv_dichvu.DataSource = dichvu.loadBang_DV();
             btn_sua.Enabled = btn_xoa.Enabled = btn_huy.Enabled = btn_luu.Enabled = txt_madichvu.Enabled
                 = txt_tendichvu.Enabled = false;
             btn_them.Enabled = true;
@@ -58,7 +59,69 @@ namespace QuanLyKhuTro.DanhMuc
 
         private void btn_luu_Click(object sender, EventArgs e)
         {
-           
+         
+            DICHVU dv = new DICHVU();
+            if (btn_them.Enabled == true)
+            {
+                dv.MADV = txt_madichvu.Text;
+                dv.TENDV = txt_tendichvu.Text;
+                dv.GIADV = Convert.ToDouble(txt_gia.Text);
+                if (txt_madichvu.Text == null && txt_tendichvu.Text == string.Empty)
+                {
+                    MessageBox.Show("không được để trống");
+                    return;
+                }
+                //kiểm tra khóa chính
+                if (dichvu.ktkc_DichVu(dv.MADV) == true)
+                {
+                    MessageBox.Show("TRùng khóa chính");
+                    return;
+                }
+                if (dichvu.them_DichVu(dv) == true)
+                {
+
+                    grv_dichvu.DataSource = dichvu.loadBang_DV();
+                    MessageBox.Show("Thành công");
+                }
+                else
+                {
+                    MessageBox.Show("Thất bại");
+                }
+                btn_sua.Enabled = btn_luu.Enabled = btn_them.Enabled = false;
+            }
+            if (btn_sua.Enabled == true)
+            {
+                try
+                {
+                    if (txt_tendichvu.Text == string.Empty)
+                    {
+                        MessageBox.Show("Tên tầng không được để trống");
+                        return;
+                    }
+                    if (txt_gia.Text == string.Empty)
+                    {
+                        MessageBox.Show("Giá dịch vụ không được để trống");
+                        return;
+                    }
+                    dv.MADV = txt_madichvu.Text;
+                    dv.TENDV = txt_tendichvu.Text;
+
+
+
+                    if (dichvu.sua_DichVu(dv) == true)
+                    {
+                        grv_dichvu.DataSource = dichvu.loadBang_DV();
+                        MessageBox.Show("Thành công");
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("thất bại");
+                }
+            }
+            btn_sua.Enabled = btn_xoa.Enabled = btn_huy.Enabled = btn_luu.Enabled =
+                txt_madichvu.Enabled = txt_tendichvu.Enabled = txt_gia.Enabled = false;
+            btn_them.Enabled = true;
         }
     }
 }
